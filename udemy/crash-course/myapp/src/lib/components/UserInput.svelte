2ccf2b3d79b7cd09465e7e471d5a65b998e0cc25
Thiago Aires da Silva <makarrao@gmail.com>
2024-12-17 23:08:32 -0300

    added udemy course

---
+++ udemy/crash-course/myapp/src/lib/components/UserInput.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+
+  interface UserInputProps{
+    children: Snippet;
+    userName: String;
+  }
+
+  let {userName, children, ...props}: UserInputProps = $props();
+  $inspect(userName);
+  function onChange(event: any){
+    console.log(`i'll send to database: ${event}`);
+  } 
+
+
+</script> 
+
+<h1>your username</h1>
+<input type="text" bind:value={userName}/>
+<p>{userName}</p>
+{@render children()}
+
+
+<style>
+
+  h1{
+      color: blue;
+      font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+
+</style>
---
+++ udemy/crash-course/myapp/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import UserInput from "$lib/components/UserInput.svelte";
+
+</script>
+<UserInput userName='bacon'>
+    <p>the amazing bacon is amazing</p>
+</UserInput>
